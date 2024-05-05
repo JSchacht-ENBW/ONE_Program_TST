6c93c277b843f56ea55c817b901b903bda904536
Jens Schacht <j.schacht@netze-bw.de>
2024-05-05 23:58:52 +0000

    Updated CloneMoveWIS.ps1

--- CloneMoveWIS.ps1
+++ CloneMoveWIS.ps1
@@ -29,10 +29,11 @@ function Create-WorkItem($workItem) {
     }
 
     # Construct the URI for creating a new work item based on the type from the existing item
-    $uri = "$baseUri/$targetProject/_apis/wit/workitems/`${$workItem.fields['System.WorkItemType']}?api-version=6.0"
+    $workItemType = $workItem.fields.'System.WorkItemType'
+    $uri = "$baseUri/$targetProject/_apis/wit/workitems/`${$workItemType}?api-version=6.0"
     
-
     # Define the body as an array of hashtables, setting title, state, and description from the submitted work item
+    # Description is escaped for JSON
     $body = @(
         @{
             "op" = "add"
@@ -46,13 +47,8 @@ function Create-WorkItem($workItem) {
         },
         @{
             "op" = "add"
-            "path" = "/fields/System.WorkItemType"
-            "value" = $workItem.fields.'System.WorkItemType'  # Set the description from the work item
-        },
-        @{
-            "op" = "add"
             "path" = "/fields/System.Description"
-            "value" = $workItem.fields.'System.Description'  # Set the description from the work item
+            "value" = $workItem.fields.'System.Description' -replace '(["\\])', '\\$1'  # Escape special JSON characters in the description
         }
     )
 
@@ -62,47 +58,6 @@ function Create-WorkItem($workItem) {
     # Execute the PATCH request with the constructed JSON body
     $response = Invoke-RestMethod -Uri $uri -Method Patch -Headers $headers -Body $jsonBody
     return $response
-}
-
-
-# Function to get all work items from the source project and area
-function Get-WorkItems {
-    $wiql = @{
-        "query" = "SELECT [System.Id], [System.WorkItemType],[System.Title], [System.State], [System.AreaPath] , [System.Description] FROM WorkItems WHERE [System.AreaPath] = '$sourceArea'"
-    }
-
-    $uri = "$baseUri/$sourceProject/_apis/wit/wiql?api-version=6.0"
-    $response = Invoke-RestMethod -Uri $uri -Method Post -Headers $headers -Body ($wiql | ConvertTo-Json -Compress)
-
-    # Check if there are work items in the response and retrieve them
-    if ($response -and $response.workItems) {
-        $ids = $response.workItems.id -join ","
-        $detailUri = "$baseUri/$sourceProject/_apis/wit/workitems?ids=$ids&`$expand=fields&api-version=6.0"
-        $workItems = Invoke-RestMethod -Uri $detailUri -Method Get -Headers $headers
-        return $workItems
-    } else {
-        Write-Host "No work items found."
-        return @()  # Return an empty array if no work items are found
-    }
-}
-
-# Main script execution
-$workItems = Get-WorkItems
-if ($workItems) {
-    foreach ($wi in $workItems.value) {
-        # Print each work item's ID and Title (assuming ID is directly under the work item object)
-        Write-Host "Work Item ID: $($wi.id), WIT: $($wi.fields.'System.WorkItemType'), Title: $($wi.fields.'System.Title'), State: $($wi.fields.'System.State'), Description: $($wi.fields.'System.Description')"
-
-         # Attempt to create a new work item in the target project using the existing work item's details
-        $newWorkItemResponse = Create-WorkItem $wi
-        if ($newWorkItemResponse) {
-            Write-Host "New work item created successfully with ID: $($newWorkItemResponse.id)"
-        } else {
-            Write-Host "Failed to create new work item."
-        }
-    }
-} else {
-    Write-Host "No work items to process."
 }
 
 
