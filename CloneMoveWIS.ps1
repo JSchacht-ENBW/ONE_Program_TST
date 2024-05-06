23a1cf3630228bf7522af67e948bedeaa528c047
Jens Schacht <j.schacht@netze-bw.de>
2024-05-06 11:08:45 +0000

    Updated CloneMoveWIS.ps1

--- CloneMoveWIS.ps1
+++ CloneMoveWIS.ps1
@@ -58,66 +58,7 @@ function Create-WorkItem($workItem) {
         }
     ]
 "@ 
-# Set variables
-$sourceOrg = "enbw"
-$sourceProject = "ONE!"
-$sourceArea = "ONE!\\xx_Sandkasten"  # Use double backslash in PowerShell for correct escaping
-$targetOrg = "enbw"
-$targetProject = "ONE! Program_Dev"
-$PAT = "hy5ljfnuzezpn5ojdasxtlhrfgopbpt3ezgrmaq5fqzsd7z4yfsa"  # Securely pass your PAT
 
-$AzureDevOpsPAT = 'hy5ljfnuzezpn5ojdasxtlhrfgopbpt3ezgrmaq5fqzsd7z4yfsa'
-$AzureDevOpsAuthenicationHeader = @{
-    Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$AzureDevOpsPAT"))
-}
-
-$OrganizationName = "enbw"
-$UriOrganization = "https://dev.azure.com/$OrganizationName/"
-
-#Lists all projects in your organization
-$uriAccount = $UriOrganization + "_apis/projects?api-version=5.1"
-Invoke-RestMethod -Uri $uriAccount -Method Get -Headers $AzureDevOpsAuthenicationHeader
-
-# Headers for authentication
-$headers = @{
-    "Authorization" = "Basic $( [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$PAT")) )"
-}
-
-# Function to create a work item in the target project
-function Create-WorkItem($workItem) {
-    $WorkItemType = $workItem.fields.'System.WorkItemType'
-    $uri = $UriOrganization + $targetProject + "/_apis/wit/workitems/`$$WorkItemType?api-version=5.1"
-    echo $uri
-
-    # Define default values for required fields to ensure they are not null
-    $title = if ($workItem.fields.'System.Title') { $workItem.fields.'System.Title' } else { "Default Title" }
-    $state = if ($workItem.fields.'System.State') { $workItem.fields.'System.State' } else { "New" }
-    $description = $workItem.fields.'System.Description'
-
-    $body = @"
-    [
-        {
-            "op": "add",
-            "path": "/fields/System.Title",
-            "value": "$title"
-        },
-        {
-            "op": "add",
-            "path": "/fields/System.State",
-            "value": "$state"
-        },
-        {
-            "op": "add",
-            "path": "/fields/System.WorkItemType",
-            "value": "$WorkItemType"
-        },
-        {
-            "op": "add",
-            "path": "/fields/System.Description",
-            "value": "$description"
-        }
-    ]
-"@ 
 
     # Headers for authentication
     $headers = @{
