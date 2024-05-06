eb369f765c9da3ee254546e6f1d68591d3afd1c2
Jens Schacht <j.schacht@netze-bw.de>
2024-05-06 02:05:35 +0000

    Updated CloneMoveWIS.ps1

--- CloneMoveWIS.ps1
+++ CloneMoveWIS.ps1
@@ -19,7 +19,7 @@ $headers = @{
 # Function to create a work item in the target project
 function Create-WorkItem($workItem) {
     $workItemType = $workItem.fields.'System.WorkItemType'
-    $uri = "$baseUri/$targetProject/_apis/wit/workitems/`$$workItemType?validateOnly=False&bypassRules=True&suppressNotifications=True&$expand=fields&api-version=7.1"
+    $uri = "$baseUri/$targetProject/_apis/wit/workitems/$($workItemType)?validateOnly=False&bypassRules=True&suppressNotifications=True&$expand=fields&api-version=7.1"
 
     # Define default values for required fields to ensure they are not null
     $title = if ($workItem.fields.'System.Title') { $workItem.fields.'System.Title' } else { "Default Title" }
@@ -60,7 +60,7 @@ function Create-WorkItem($workItem) {
         Write-Host "Request failed with the following details:"
         Write-Host "Status Code: $($_.Exception.Response.StatusCode.Value__)"
         Write-Host "Status Description: $($_.Exception.Response.StatusDescription)"
-        Write-Host "Body: $($body)"
+        Write-Host "Body: $($jsonBody)"
         Write-Host "URI: $($uri)"
         
         # Check if the content can be converted to JSON
