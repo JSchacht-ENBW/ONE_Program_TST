b8db19cf570cf4c7b4b4e81c8602d58563668bb1
Jens Schacht <j.schacht@netze-bw.de>
2024-05-06 05:35:46 +0000

    Added SimpleCreateWI.ps1

---
+++ SimpleCreateWI.ps1
@@ -0,0 +1,31 @@
+﻿#Authentication in Azure DevOps
+$AzureDevOpsPAT = 'hy5ljfnuzezpn5ojdasxtlhrfgopbpt3ezgrmaq5fqzsd7z4yfsa'
+$AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($AzureDevOpsPAT)")) }
+
+$OrganizationName = "enbw"
+$UriOrganization = "https://dev.azure.com/$($OrganizationName)/"
+
+#Lists all projects in your organization
+$uriAccount = $UriOrganization + "_apis/projects?api-version=5.1"
+Invoke-RestMethod -Uri $uriAccount -Method get -Headers $AzureDevOpsAuthenicationHeader 
+
+
+#Create a work item
+
+$WorkItemType = "task"
+$WorkItemTitle = "Test from Powershell"
+$ProjectName = "ONE! Program_De";
+
+
+$uri = $UriOrganization + $ProjectName + "/_apis/wit/workitems/$" + $WorkItemType + "?api-version=5.1"
+echo $uri
+
+$body="[
+  {
+    `"op`": `"add`",
+    `"path`": `"/fields/System.Title`",
+    `"value`": `"$($WorkItemTitle)`"
+  }
+]"
+
+Invoke-RestMethod -Uri $uri -Method POST -Headers $AzureDevOpsAuthenicationHeader -ContentType "application/json-patch+json" -Body $body
