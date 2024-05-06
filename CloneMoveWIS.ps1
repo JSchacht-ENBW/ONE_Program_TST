d9df289f41ebcc603d363656189ea8fcc1887ad5
Jens Schacht <j.schacht@netze-bw.de>
2024-05-06 03:38:52 +0000

    Updated CloneMoveWIS.ps1

--- CloneMoveWIS.ps1
+++ CloneMoveWIS.ps1
@@ -22,7 +22,7 @@ function Create-WorkItem($workItem) {
     $workItemType_ = "`$$($workItemType)"  # Prepend $ and handle it as a literal part of the string
     # URI encode the work item type to handle spaces and special characters
     $encodedWorkItemType = [System.Web.HttpUtility]::UrlEncode($workItemType)
-    $uri = "$baseUri/$targetProject/_apis/wit/workitems/$Feature?validateOnly=False&bypassRules=True&suppressNotifications=True&`$expand=fields&api-version=7.1"
+    $uri = "$baseUri/$targetProject/_apis/wit/workitems/`$$Feature?validateOnly=False&bypassRules=True&suppressNotifications=True&`$expand=fields&api-version=7.1"
 
 
     # Define default values for required fields to ensure they are not null
