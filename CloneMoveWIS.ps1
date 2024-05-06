027bc4a7ea59010d898dab8d3563f23e25bf5b53
Jens Schacht <j.schacht@netze-bw.de>
2024-05-06 03:15:39 +0000

    Updated CloneMoveWIS.ps1

--- CloneMoveWIS.ps1
+++ CloneMoveWIS.ps1
@@ -20,10 +20,10 @@ $headers = @{
 function Create-WorkItem($workItem) {
     $workItemType = $workItem.fields.'System.WorkItemType'
     # Properly encode the work item type to handle spaces and special characters
-    $encodedWorkItemType = [uri]::EscapeDataString($workItemType)
+    $formattedWorkItemType = $workItemType -replace ' ', '%20'
 
-    # Construct the URI with correct PowerShell string formatting
-    $uri = "$baseUri/$targetProject/_apis/wit/workitems/${encodedWorkItemType}?validateOnly=False&bypassRules=True&suppressNotifications=True&`$expand=fields&api-version=7.1"
+    # Construct the URI, explicitly adding a `%` before the work item type if needed
+    $uri = "$baseUri/$targetProject/_apis/wit/workitems/%$formattedWorkItemType?validateOnly=False&bypassRules=True&suppressNotifications=True&`$expand=fields&api-version=7.1"
 
     # Define default values for required fields to ensure they are not null
     $title = if ($workItem.fields.'System.Title') { $workItem.fields.'System.Title' } else { "Default Title" }
